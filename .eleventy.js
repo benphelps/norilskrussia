0e14c7344caa547d08e3e45dc12621e59406b306
Ben Phelps <ben@phelps.io>
2020-03-25 22:16:50 +0200

    cleanup source, add meta tags, convert to sass

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,11 @@
+const pluginSass = require("eleventy-plugin-sass");
+
 module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy("img");
     eleventyConfig.addPassthroughCopy("css");
     eleventyConfig.addPassthroughCopy("favicon.ico");
 
+    eleventyConfig.addPlugin(pluginSass);
 
     return {
         passthroughFileCopy: true
